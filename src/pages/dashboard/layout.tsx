d7fed855b6385bd7e173e2f796bbd3bcdc722969
Amar <amar@Amars-MacBook-Pro.local>
2024-08-22 01:56:19 +0530

    Slide over completed

--- src/pages/dashboard/layout.tsx
+++ src/pages/dashboard/layout.tsx
@@ -10,15 +10,17 @@ import { AlignJustify } from "lucide-react"
 
 import { ReactNode } from "react";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "@/context/authContext";
 
 type Props = {
     children: ReactNode;
 };
 
 const DashboardLayout: React.FC<Props> = ({ children }) => {
+    const authContext = useAuth();
     const navigate = useNavigate()
     return (
-        <div>
+        <div className=" text-lg">
             <nav className="flex items-center justify-between px-4 py-2 h-[7vh] bg-white dark:bg-gray-800">
                 <a href="#" className="flex items-center gap-2" >
                     <img
@@ -32,28 +34,45 @@ const DashboardLayout: React.FC<Props> = ({ children }) => {
                 <Sheet>
                     <SheetTrigger asChild>
                         <Button variant="link" size="icon" className="">
-                            <AlignJustify className="h-10 w-10 text-primary" />
+                            <AlignJustify className="h-10 w-10 text-brand" />
                             <span className="sr-only">Toggle navigation menu</span>
                         </Button>
                     </SheetTrigger>
                     <SheetContent side="right" className="border-0 rounded-l-3xl">
-                        <div className="grid w-full p-4 debug">
-                            <a href="#" className="text-lg font-medium hover:underline underline-offset-4" >
-                                Home
-                            </a>
-                            <a href="#" className="text-lg font-medium hover:underline underline-offset-4" >
-                                About
-                            </a>
-                            <a href="#" className="text-lg font-medium hover:underline underline-offset-4" >
-                                Services
-                            </a>
-                            <a href="#" className="text-lg font-medium hover:underline underline-offset-4" >
-                                Portfolio
-                            </a>
-                            <a href="#" className="text-lg font-medium hover:underline underline-offset-4" >
-                                Contact
-                            </a>
+                        <div className="w-full flex flex-col gap-6">
+                            <div className="flex items-center gap-4 text-brand">
+                                <div className="rounded-full border-2 border-brand">
+                                    <img src="https://via.placeholder.com/75" alt="" className="rounded-full" width={75} height={75} />
+                                </div>
+                                <div>
+                                    <p className="text-lg font-medium">{authContext?.userData?.company}</p>
+                                    <p className="text-sm font-medium">{authContext?.userData?.email}</p>
+                                </div>
+                            </div>
+                            <div className="flex justify-between items-center gap-4 bg-brand text-white p-2 rounded-3xl">
+                                <div>
+                                    <p className="text-xl font-medium">${authContext?.userData?.balance ?? 76786}</p>
+                                    <p className="text-lg">Current balance</p>
+                                </div>
+                                <div>
+                                    <Button variant="link" className="bg-white" onClick={() => navigate('/dashboard/transactions')}>Manage</Button>
+                                </div>
+                            </div>
+                            <div className="flex flex-col gap-4">
+                                <a href="#" className="text-lg font-medium hover:underline underline-offset-4 p-2 text-brand hover:bg-brand/20 rounded-3xl" >
+                                    Complete transactions
+                                </a>
+                                <a href="#" className="text-lg font-medium hover:underline underline-offset-4 p-2 text-brand hover:bg-brand/20 rounded-3xl" >
+                                    Manage payment methods
+                                </a>
+                                <a href="#" className="text-lg font-medium hover:underline underline-offset-4 p-2 text-brand hover:bg-brand/20 rounded-3xl" >
+                                    Manage subscription
+                                </a>
+                            </div>
                         </div>
+                        <footer className="mt-[10vh] text-center text-sm">
+                            <p>Not you? <span><a href="#" className="text-brand hover:underline" >Logout</a></span></p>
+                        </footer>
                     </SheetContent>
                 </Sheet>
             </nav>
